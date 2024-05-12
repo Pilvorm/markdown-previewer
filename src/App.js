730825da1fad1b94b5528b17524aea5311683b54
DANNNN <danielemerald@ymail.com>
2024-05-12 16:14:25 +0700

    fixed dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import React from 'react';
-import { marked } from 'marked';
-import Prism from 'prismjs';
-import { motion } from 'framer-motion';
-
+import "./App.css";
+import React from "react";
+import { marked } from "marked";
+import Prism from "prismjs";
+import { motion } from "framer-motion";
 
 marked.setOptions({
   breaks: true,
   highlight: function (code) {
-    return Prism.highlight(code, Prism.languages.javascript, 'javascript');
-  }
-})
+    return Prism.highlight(code, Prism.languages.javascript, "javascript");
+  },
+});
 
 const renderer = new marked.Renderer();
 renderer.link = function (href, title, text) {
@@ -25,7 +24,7 @@ class App extends React.Component {
       lightTheme: true,
       markdown: placeholder,
       editorMaximized: false,
-      previewMaximized: false
+      previewMaximized: false,
     };
 
     this.handleTheme = this.handleTheme.bind(this);
@@ -36,58 +35,62 @@ class App extends React.Component {
 
   handleTheme() {
     this.setState({
-      lightTheme: !this.state.lightTheme
+      lightTheme: !this.state.lightTheme,
     });
   }
 
   handleChange(e) {
     this.setState({
-      markdown: e.target.value
+      markdown: e.target.value,
     });
   }
 
   handleEditorMaximize() {
     this.setState({
-      editorMaximized: !this.state.editorMaximized
+      editorMaximized: !this.state.editorMaximized,
     });
   }
+
   handlePreviewMaximize() {
     this.setState({
-      previewMaximized: !this.state.previewMaximized
+      previewMaximized: !this.state.previewMaximized,
     });
   }
 
   render() {
-
-    const themeClass = this.state.lightTheme ? ['light', 'dark_mode'] : ['dark', 'light_mode'];
+    const themeClass = this.state.lightTheme
+      ? ["light", "dark_mode"]
+      : ["dark", "light_mode"];
 
     const classes = this.state.editorMaximized
-      ? ['editor-side maximized', 'preview-side hide', 'close_fullscreen']
+      ? ["editor-side maximized", "preview-side hide", "close_fullscreen"]
       : this.state.previewMaximized
-        ? ['editor-side hide', 'preview-side maximized', 'close_fullscreen']
-        : ['editor-side', 'preview-side', 'open_in_full'];
+      ? ["editor-side hide", "preview-side maximized", "close_fullscreen"]
+      : ["editor-side", "preview-side", "open_in_full"];
 
     return (
-      <motion.div className={"App"}>
-        <Header
-          onClick={this.handleTheme}
-          themeIcon={themeClass[1]}
-        />
-        <motion.div className="main"
+      <motion.div className={`App ${themeClass[0]}`}>
+        <Header onClick={this.handleTheme} themeIcon={themeClass[1]} />
+        <motion.div
+          className="main"
           initial={{ opacity: 0, scale: 1 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.75 }}>
-
+          transition={{ duration: 0.75 }}
+        >
           <div className={classes[0]}>
             <Toolbar
               icon="edit"
               sizing={classes[2]}
               onClick={this.handleEditorMaximize}
               text="Editor"
+              theme={themeClass[0]}
             />
 
-            <Editor markdown={this.state.markdown} onChange={this.handleChange} />
-
+            <Editor
+              markdown={this.state.markdown}
+              onChange={this.handleChange}
+              theme={themeClass[0]}
+            />
           </div>
 
           <div className={classes[1]}>
@@ -96,11 +99,11 @@ class App extends React.Component {
               sizing={classes[2]}
               onClick={this.handlePreviewMaximize}
               text="Preview"
+              theme={themeClass[0]}
             />
 
             <Preview markdown={this.state.markdown} />
           </div>
-
         </motion.div>
       </motion.div>
     );
@@ -115,21 +118,30 @@ const Header = (props) => {
         <p>by Daniel Emerald</p>
       </div>
       <div className="action">
-        <motion.span className="material-symbols-outlined" onClick={props.onClick}
-          whileHover={{ scale: 1.2, rotate: 30 }}>
+        <motion.span
+          className="material-symbols-outlined"
+          onClick={props.onClick}
+          whileHover={{ scale: 1.2, rotate: 30 }}
+        >
           {props.themeIcon}
         </motion.span>
       </div>
     </header>
   );
-}
+};
 
 const Toolbar = (props) => {
   return (
-    <div className="toolbar">
+    <div className={`toolbar toolbar-${props.theme}`}>
       <span className="material-symbols-outlined tool-icon">{props.icon}</span>
       {props.text}
-      <span className="material-symbols-outlined cursor-pointer sizing" onClick={props.onClick}>{props.sizing}</span>
+      <span
+        className={`material-symbols-outlined cursor-pointer sizing`}
+        onClick={props.onClick}
+        style={{color: props.theme === "dark" ? "white" : ""}}
+      >
+        {props.sizing}
+      </span>
     </div>
   );
 };
@@ -141,6 +153,7 @@ const Editor = (props) => {
       onChange={props.onChange}
       type="text"
       value={props.markdown}
+      className={props.theme}
     />
   );
 };
@@ -149,7 +162,7 @@ const Preview = (props) => {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: marked(props.markdown, { renderer: renderer })
+        __html: marked(props.markdown, { renderer: renderer }),
       }}
       id="preview"
     />
